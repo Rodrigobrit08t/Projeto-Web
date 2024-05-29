ddc651cfb29d3528a975119bc2e513f6a1297455
Rodrigo Brito <rodrigobrit08t@gmail.com>
2024-05-29 10:01:01 -0300

    Criando o CRUD do projeto

--- src/JS/Main.js
+++ src/JS/Main.js
@@ -2,21 +2,19 @@
 
 const song = {artist:"", title:"", lyrics:""}
 
-function addSong({artist, title, lyrics}){
+function addCard({artist, title, lyrics}){
     const main = document.querySelector('body > main')
 
     main.innerHTML += `
         <div class="card-song" id="${artist}" onmouseenter="cardEnter(event)" onmouseleave="cardLeave(event)">
 			<header>
-				<h2>${Artist}</h2>
-				<h1>${Name}</h1>
+				<h2><span>Artista: </span>${artist}</h2>
+				<h1><span>Título: </span>${title}</h1>
 			</header>
 			<main>
-				<p>${Lyrics}</p>
+				<h3><span>Letra:</span><br></h3>
+				<p>${lyrics}</p>
 			</main>
-			<footer>
-			
-			</footer>
 			<div class="card-menu">
 				<span>Editar</span>
 				<span onclick="removeCard(event)">Excluir</span>
@@ -27,6 +25,30 @@ function addSong({artist, title, lyrics}){
 	allEdit.forEach(edit => {
 		edit.addEventListener('click', openEditModal)
 	})
+}
+
+function updateCard({artist, title, lyrics}){
+
+    const card = document.querySelector(`#${artist}`)
+    
+	card.innerHTML = `
+			<header>
+				<h2><span>Artista: </span>${artist}</h2>
+				<h1><span>Título: </span>${title}</h1>
+			</header>
+			<main>
+				<h3><span>Letra:</span><br></h3>
+				<p>${lyrics}</p>
+			</main>
+			<div class="card-menu">
+				<span>Editar</span>
+				<span onclick="removeCard(event)">Excluir</span>
+			</div>
+		</div>
+    `
+
+	const edit = card.querySelector('.card-song .card-menu span:first-child')
+	edit.addEventListener('click', openEditModal)
 }
 
 const openModal = (idModal) => {
@@ -49,5 +71,62 @@ const closeModal = (event, id) => {
 }
 
 function loadCards(){
-	song.map(stock => addSong(stock))
+	song.map(stock => addCard(stock))
 }
+
+const createCard = (event) =>{
+	event.preventDefault()
+	const formData = new FormData(event.target)
+	const stock = Object.fromEntries(formData)
+
+	addCard(stock)
+	event.target.reset()
+	closeModal(null, 'add-form-modal')
+}
+
+const cardEnter = (event) => {
+	const cardMenu = event.target.querySelector('.card-menu')
+	cardMenu.style.display = 'flex'
+}
+
+const cardLeave = (event) => {
+	const cardMenu = event.target.querySelector('.card-menu')
+	cardMenu.style.display = 'none'
+}
+
+const removeCard = (event) => {
+	event.target.closest('.card-song').remove()
+}
+
+
+const openEditModal = (event) => {
+	const card = event.target.closest('.card-song')
+
+	const inputTitle = document.getElementById('e-title')
+	inputTitle.value = card.querySelector('header h1 span').innerText.replace(':', '')
+
+	const inputArtist = document.getElementById('e-artist')
+	const inputOldArtist = document.getElementById('e-old-artist')
+	inputArtist.value = card.querySelector('header h2').innerText.replace(':', '')
+	inputOldArtist.value = card.querySelector('header h2').innerText.replace(':', '')
+
+	const inputLyrics = document.getElementById('e-lyrics')
+	inputLyrics.value = card.querySelector('main p').innerText
+
+	openModal('edit-form-modal')
+}
+
+const editCard = (event) =>{
+	event.preventDefault()
+	const formData = new FormData(event.target)
+	const stock = Object.fromEntries(formData)
+
+	const card = document.getElementById(stock['old-artist'])
+	card.setAttribute('id', stock.codigo)
+
+	updateCard({
+		...stock
+	})
+	closeModal(null, 'edit-form-modal')
+}
+
